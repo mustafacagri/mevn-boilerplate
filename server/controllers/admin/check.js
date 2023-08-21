cc75ccfc4e9044d473a882612aa403559eb2f712
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2023-08-21 22:41:16 +0300

    refactor: respond with a meaningful message if the user is an admin

--- server/controllers/admin/check.js
+++ server/controllers/admin/check.js
@@ -2,5 +2,5 @@ const { response } = require('../../classes')
 const { controllers: { check: STRINGS } = {} } = require('../../MAGIC_STRINGS')
 
 exports.check = async (req, res) => {
-  response.successed(res, null)
+  response.successed(res, null, STRINGS.adminUser)
 }
