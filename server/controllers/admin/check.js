1f59f2af80169b936fda5d99ea95f896867793b8
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2023-09-23 23:57:36 +0300

    refactor(server): do not return a message for the admin check

--- server/controllers/admin/check.js
+++ server/controllers/admin/check.js
@@ -2,5 +2,5 @@ const { response } = require('../../classes')
 const { controllers: { check: STRINGS } = {} } = require('../../MAGIC_STRINGS')
 
 exports.check = async (req, res) => {
-  response.successed(res, null, STRINGS.adminUser)
+  response.successed(res, null, null)
 }
