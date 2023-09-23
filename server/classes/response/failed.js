01269bad49e3a08c4c839440fb34581355945480
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2023-09-23 23:57:07 +0300

    refactor(server): return list of the error messages in an array

--- server/classes/response/failed.js
+++ server/classes/response/failed.js
@@ -1,5 +1,9 @@
 const fail = require('./fail')
 
 module.exports = (res, message, status = 200) => {
+  if (typeof message === 'object') {
+    message = `<ul><li>${message.join('</li><li>')}</li></ul>`
+  }
+
   return res.status(status).json(new fail(message))
 }
